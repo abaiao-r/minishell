39e37767943dfa4f029a096169aa1b5959232405
IcQuackson <pedrorenato1998@gmail.com>
2023-04-23 21:45:00 +0100

    changed green to orange

--- src/print_prompt.c
+++ src/print_prompt.c
@@ -6,7 +6,7 @@
 /*   By: quackson <quackson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 18:37:02 by abaiao-r          #+#    #+#             */
-/*   Updated: 2023/04/23 12:38:00 by quackson         ###   ########.fr       */
+/*   Updated: 2023/04/23 21:43:47 by quackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*print_prompt(void)
 	username = getenv("USER");
 	cwd = getcwd(NULL, 0);
 	//printf("%s@minishell:%s", username, cwd);
-	printf("\033[1;32m%s@minishell:", username);  // bright green username
+	printf("\033[38;5;208m%s@minishell:", username);  // bright green username
     printf("\033[1;34m%s", cwd);  // blue cwd
     printf("\033[0m");  // reset color
 	input = readline("$ ");
