719101e3a255984f15749410b002a464b62b0754
Andre Francisco Baiao Rolao Candido Da Silva <abaiao-r@c1r16s9.42lisboa.com>
2023-04-24 15:25:56 +0100

    fix ft_split_default in utils_2.c; fix command_utils where path was path=path;

--- src/utils_2.c
+++ src/utils_2.c
@@ -71,59 +71,6 @@ char	**ft_split_default(char *str)
 	return (out);
 }
 
-/* char	**ft_split_1(char *str)
-{
-	int		i;
-	int		j;
-	int		k;
-	char	**out;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	out = (char **)malloc(sizeof(char *) * (count_words(str) + 1));
-	while (str[i])
-	{
-		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
-			i++;
-		j = i;
-		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
-			i++;
-		if (i > j)
-		{
-			out[k] = (char *)malloc(sizeof(char) * ((i - j) + 1));
-			ft_strncpy(out[k++], &str[j], i - j);
-		}
-	}
-	out[k] = NULL;
-	return (out);
-}
-
-#include <ctype.h>
-
-char **split(const char *string) {
-	int length = 0, count = 0, i = 0, j = 0;
-	while(*(string++)) {
-		if (*string == ' ') count++;
-		length++;
-	}
-	string -= (length + 1); // string was incremented one more than length
-	char **array = (char **)malloc(sizeof(char *) * (length + 1));
-	char ** base = array;
-	for(i = 0; i < (count + 1); i++) {
-		j = 0;
-		while(string[j] != ' ') j++;
-		j++;
-		*array = (char *)malloc(sizeof(char) * j);
-		memcpy(*array, string, (j-1));
-		(*array)[j-1] = '\0';
-		string += j;
-		array++;
-	}
-	*array = '\0';
-	return base;  
-} */
-
 
 /* main para testar o ft_split_default */
 /* int	main(void)
