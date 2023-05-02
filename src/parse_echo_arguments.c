5d32b22bf25dd291a050d483ce81c091d6869b1a
Andre Francisco Baiao Rolao Candido Da Silva <abaiao-r@c1r18s7.42lisboa.com>
2023-05-02 19:47:08 +0100

    char **parse_echo_arguments

--- src/parse_echo_arguments.c
+++ src/parse_echo_arguments.c
@@ -6,7 +6,7 @@
 /*   By: abaiao-r <abaiao-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 18:38:19 by abaiao-r          #+#    #+#             */
-/*   Updated: 2023/05/02 19:09:19 by abaiao-r         ###   ########.fr       */
+/*   Updated: 2023/05/02 19:46:08 by abaiao-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ void	free_memory(char **args, size_t count)
 	free(args);
 }
 
-char	**parse_arguments(char *string, char **args, size_t count)
+/* char	**parse_arguments(char *string, char **args, size_t count)
 {
 	char	*arg;
 
@@ -68,11 +68,76 @@ char	**parse_arguments(char *string, char **args, size_t count)
 			++count;
 			while (*string != '\0' && !isspace(*string))
 				++string;
-			args[count - 1] = strndup(arg, string - arg);
+			args[count - 1] = ft_strndup(arg, string - arg);
 			if (!args[count - 1])
 			{
 				free_memory(args, count - 1);
 				return (NULL);
+			}
+		}
+	}
+	args[count] = NULL;
+	return (args);
+} */
+int	is_quote(char c)
+{
+	return (c == '\'' || c == '\"');
+}
+
+char	**parse_arguments(char *string, char **args, size_t count)
+{
+	char	*arg;
+	int		inside_quote;
+	char	quote_type;
+	char	*arg_str;
+
+	inside_quote = 0;
+	quote_type = '\0';
+	while (*string != '\0')
+	{
+		while (*string != '\0' && isspace(*string))
+			++string;
+		if (*string != '\0')
+		{
+			arg = string;
+			++count;
+			if (is_quote(*string))
+			{
+				inside_quote = 1;
+				quote_type = *string;
+				++string;
+			}
+			while (*string != '\0')
+			{
+				if (inside_quote && *string == quote_type)
+				{
+					inside_quote = 0;
+					quote_type = '\0';
+					++string;
+					break ;
+				}
+				else if (!inside_quote && isspace(*string))
+				{
+					++string;
+					break ;
+				}
+				++string;
+			}
+			if (arg != string)
+			{
+				arg_str = strndup(arg, string - arg);
+				if (!arg_str)
+				{
+					free_memory(args, count - 1);
+					return (NULL);
+				}
+				if (is_quote(arg_str[0])
+					&& arg_str[0] == arg_str[strlen(arg_str) - 1])
+				{
+					arg_str[strlen(arg_str) - 1] = '\0';
+					++arg_str;
+				}
+				args[count - 1] = arg_str;
 			}
 		}
 	}
@@ -100,10 +165,10 @@ char	**parse_echo_arguments(char *string)
 	return (args);
 }
 
-/* int	main(void)
+int	main(void)
 {
-    int i;
-	char input[] = "Hello \"World is\" is a test.";
+	int i;
+	char input[] = "Hello \'World is is a test.";
 	char **args = parse_echo_arguments(input);
 
 	if (!args)
@@ -111,12 +176,12 @@ char	**parse_echo_arguments(char *string)
 		fprintf(stderr, "Error: failed to parse input.\n");
 		return (EXIT_FAILURE);
 	}
-    i = 0;
+	i = 0;
 	while (args[i] != NULL)
 	{
 		printf("Argument %d: %s\n", i, args[i]);
-        i++;
+		i++;
 	}
 	free(args);
 	return (EXIT_SUCCESS);
-} */
+}
