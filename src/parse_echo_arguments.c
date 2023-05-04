d343dcf11d2b2d8649ca0bf9d9ee76d37d074d59
Andre Francisco Baiao Rolao Candido Da Silva <abaiao-r@c1r13s3.42lisboa.com>
2023-05-04 17:13:59 +0100

    char **parse_echo_arguments separates words with space and ignores closed quotes but still things to do: (1) Norm; (2) Not interpret unclosed quotes or special characters which are not required by thesubject such as \ (backslash) or ; (semicolon); (3) Handle ’ (single quote) which should prevent the shell from interpreting the meta-characters in the quoted sequence; (4) Handle " (double quote) which should prevent the shell from interpreting the metacharacters in the quoted sequence except for $ (dollar sign).

--- src/parse_echo_arguments.c
+++ src/parse_echo_arguments.c
@@ -6,7 +6,7 @@
 /*   By: abaiao-r <abaiao-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 18:38:19 by abaiao-r          #+#    #+#             */
-/*   Updated: 2023/05/04 15:16:17 by abaiao-r         ###   ########.fr       */
+/*   Updated: 2023/05/04 17:13:33 by abaiao-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	ft_skip_whitespace(char **str)
 		++i;
 	}
 	*str += i;
-} */
+}
 
 void	free_memory(char **args, size_t count)
 {
@@ -50,6 +50,17 @@ void	free_memory(char **args, size_t count)
 	}
 	free(args);
 }
+
+char	*ft_strncpy(char *s1, char *s2, int n)
+{
+	int	i;
+
+	i = -1;
+	while (++i < n && s2[i])
+		s1[i] = s2[i];
+	s1[i] = '\0';
+	return (s1);
+} */
 
 /* utils | */
 
@@ -90,8 +101,10 @@ int	count_words_parser(char *str)
 				i++;
 			i++;
 		}
-		else if (str[i])
+		else if (str[i] && (str[i] != ' ' || str[i] != '\t' || str[i] != '\n'))
 			wc++;
+		while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+            i++;
 	}
 	return (wc);
 }
@@ -195,7 +208,7 @@ the funtions need were pasted below this main */
 /* main to test count_words_parser*/
 /* int main(void)
 {
-	char input[] = "Hello \'World is ae\' \"a test.\"";
+	char input[] = "Hello \"World is\" ae \'a test.\'";
 
 	printf("%d", count_words_parser(input));
 } */
