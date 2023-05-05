ffd489048ce7e5cf0b82cf849f6c011abb25b1ec
Andre Francisco Baiao Rolao Candido Da Silva <abaiao-r@c1r13s3.42lisboa.com>
2023-05-05 21:48:26 +0100

    fixing export

--- src/export.c
+++ src/export.c
@@ -6,7 +6,7 @@
 /*   By: abaiao-r <abaiao-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 21:58:38 by abaiao-r          #+#    #+#             */
-/*   Updated: 2023/05/05 19:52:49 by abaiao-r         ###   ########.fr       */
+/*   Updated: 2023/05/05 21:47:51 by abaiao-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,27 +63,45 @@ int	show_export(t_env **environment)
 	return (NO_EXIT);
 }
 
+int	update_env_name(t_env *env_list, char *find_var, char *new_value)
+{
+	t_env	*current_node;
+
+	current_node = env_list;
+	while (current_node != NULL)
+	{
+		if (ft_strcmp(current_node->var_name, find_var) == 0)
+		{
+			current_node->var_value = new_value;
+			return (1);
+		}
+		current_node = current_node->next;
+	}
+	return (0);
+}
+
 int	export(char **input, int num_tokens, t_env **environment)
 {
 	int	i;
+	char *equal_sign;
 
+	i = 1;
 	if (num_tokens == 1)
 		return (show_export(environment));
-	if (num_tokens != 2 || input[1][0] == '_')
-		return (NO_EXIT);
-	if (input[1][0] == '_')
-		return (NO_EXIT);
-	if (!ft_isalpha(input[1][0]))
-		return (export_error(input[1]));
-	i = 0;
-	while (input[1][i] && input[1][i] != '=')
+	if ((!ft_isalpha(input[1][0]) && !(input[1][0] == '_')))
 	{
-		if (!ft_isalnum(input[1][i]) && input[1][i] != '_')
-			return (export_error(input[1]));
-		i++;
+		printf("export: `%s': not a valid identifier \n", input[i]);
+		return ;
 	}
-	if (ft_strchr(input[1], '='))
-		ft_setenv(input[1], environment);
+	if (!ft_strchr(input[i], '='))
+		return ;
+	char *equal_sign = strchr(input, '=');
+	find_var = 
+	new_value = 
+	update_env_name(environment, find_var, new_value);
+	
+	/* if (ft_strchr(input[1], '='))
+		ft_setenv(input[1], environment); */
 	//ft_putenv(input, env);
 	return (NO_EXIT);
 }
--- src/ft_setenv.c
+++ src/ft_setenv.c
@@ -6,14 +6,14 @@
 /*   By: abaiao-r <abaiao-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 18:58:18 by abaiao-r          #+#    #+#             */
-/*   Updated: 2023/05/05 19:53:11 by abaiao-r         ###   ########.fr       */
+/*   Updated: 2023/05/05 21:03:13 by abaiao-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void    ft_setenv(char **input, t_env **environment)
+/* void    ft_setenv(char **input, t_env **environment)
 {
 
     
-}
+} */
