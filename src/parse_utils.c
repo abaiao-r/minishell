0f3e1f2fbedcabdb249e054ea0f32f79c39827fe
Andre Francisco Baiao Rolao Candido Da Silva <abaiao-r@c1r13s3.42lisboa.com>
2023-05-16 18:00:13 +0100

    parse_utils.c: fixed norm

--- src/parse_utils.c
+++ src/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: abaiao-r <abaiao-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 17:56:16 by abaiao-r          #+#    #+#             */
-/*   Updated: 2023/05/16 17:24:39 by abaiao-r         ###   ########.fr       */
+/*   Updated: 2023/05/16 17:59:32 by abaiao-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 typedef struct s_dollar_data
 {
-	int start;
-	int end;
-	char *find_var;
-	char *replacement;
-	size_t input_len;
-	size_t replacement_len;
-	int flag_double_quotes;
-	int flag_single_quotes;
-} t_dollar_data;
+	int		start;
+	int		end;
+	char	*find_var;
+	char	*replacement;
+	size_t	input_len;
+	size_t	replacement_len;
+	int		flag_double_quotes;
+	int		flag_single_quotes;
+}			t_dollar_data;
 
 static char	*search_and_replace_var(t_env **env_list, char *find_var,
 		int flag_single_quotes)
@@ -37,7 +37,7 @@ static char	*search_and_replace_var(t_env **env_list, char *find_var,
 			&& flag_single_quotes == 0)
 		{
 			var_value = ft_strndup((const char *)current_node->var_value,
-									ft_strlen(current_node->var_value));
+					ft_strlen(current_node->var_value));
 			return (var_value);
 		}
 		current_node = current_node->next;
@@ -64,10 +64,52 @@ static void	update_quote_flags(char current_char, int *flag_single_quotes,
 	}
 }
 
-
-char *parse_dollar(char *input, t_env **environment)
+static void	replace_input(char **input, t_dollar_data *dollar_data)
 {
-	size_t i;
+	size_t	new_input_len;
+	char	*new_input;
+
+	dollar_data->replacement_len = ft_strlen(dollar_data->replacement);
+	new_input_len = dollar_data->input_len + dollar_data->replacement_len
+		- (dollar_data->end - dollar_data->start - 1);
+	new_input = malloc(new_input_len + 1);
+	ft_memcpy(new_input, *input, dollar_data->start);
+	ft_memcpy(new_input + dollar_data->start, dollar_data->replacement,
+		dollar_data->replacement_len);
+	ft_memcpy(new_input + dollar_data->start + dollar_data->replacement_len,
+		&(*input)[dollar_data->end],
+		dollar_data->input_len - dollar_data->end + 1);
+	free(*input);
+	*input = new_input;
+	dollar_data->input_len = new_input_len;
+}
+
+static void	replace_dollar_var(char **input, t_env **environment,
+		t_dollar_data *dollar_data, size_t *i)
+{
+	dollar_data->start = *i;
+	(*i)++;
+	while (*i < dollar_data->input_len && (*input)[*i] != ' '
+		&& (*input)[*i] != '$' && (*input)[*i] != '\"' && (*input)[*i] != '\'')
+		(*i)++;
+	dollar_data->end = *i;
+	dollar_data->find_var = ft_strndup(&(*input)[dollar_data->start + 1],
+			dollar_data->end - dollar_data->start - 1);
+	dollar_data->replacement = search_and_replace_var(environment,
+			dollar_data->find_var,
+			dollar_data->flag_single_quotes);
+	if (dollar_data->replacement != NULL)
+	{
+		replace_input(input, dollar_data);
+		*i = dollar_data->start + dollar_data->replacement_len;
+	}
+	free(dollar_data->find_var);
+	free(dollar_data->replacement);
+}
+
+char	*parse_dollar(char *input, t_env **environment)
+{
+	size_t			i;
 	t_dollar_data	*dollar_data;
 
 	i = 0;
@@ -75,43 +117,16 @@ char *parse_dollar(char *input, t_env **environment)
 	dollar_data->input_len = ft_strlen(input);
 	dollar_data->flag_single_quotes = 0;
 	dollar_data->flag_double_quotes = 0;
-
-    while (i < dollar_data->input_len)
-    {
-        if (input[i] == '\'' || input[i] == '\"')
-            update_quote_flags(input[i], &(dollar_data->flag_single_quotes),
-                    &(dollar_data->flag_double_quotes));
-        if (input[i] == '$')
-        {
-            dollar_data->start = i;
-            i++;
-            while (i < dollar_data->input_len && input[i] != ' ' && input[i] != '$'
-                   && input[i] != '\"' && input[i] != '\'')
-                i++;
-            dollar_data->end = i;
-            dollar_data->find_var = ft_strndup(&input[dollar_data->start + 1], dollar_data->end - dollar_data->start - 1);
-            dollar_data->replacement = search_and_replace_var(environment, dollar_data->find_var,
-                                                              dollar_data->flag_single_quotes);
-            if (dollar_data->replacement != NULL)
-            {
-                dollar_data->replacement_len = ft_strlen(dollar_data->replacement);
-                size_t new_input_len = dollar_data->input_len + dollar_data->replacement_len - (dollar_data->end - dollar_data->start - 1);
-                char *new_input = malloc(new_input_len + 1);
-                ft_memcpy(new_input, input, dollar_data->start);
-                ft_memcpy(new_input + dollar_data->start, dollar_data->replacement, dollar_data->replacement_len);
-                ft_memcpy(new_input + dollar_data->start + dollar_data->replacement_len, &input[dollar_data->end],
-                          dollar_data->input_len - dollar_data->end + 1);
-                free(input);
-                input = new_input;
-                dollar_data->input_len = new_input_len;
-                i = dollar_data->start + dollar_data->replacement_len;
-            }
-            free(dollar_data->find_var);
-            free(dollar_data->replacement);
-        }
-        else
-            i++;
-    }
+	while (i < dollar_data->input_len)
+	{
+		if (input[i] == '\'' || input[i] == '\"')
+			update_quote_flags(input[i], &(dollar_data->flag_single_quotes),
+				&(dollar_data->flag_double_quotes));
+		if (input[i] == '$')
+			replace_dollar_var(&input, environment, dollar_data, &i);
+		else
+			i++;
+	}
 	free(dollar_data);
 	return (input);
 }
